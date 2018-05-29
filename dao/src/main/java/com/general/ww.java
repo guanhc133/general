2c649361b676f80de25b090c2d2a011e10b2e597
guanhongcheng <guanhongcheng-xf@bestpay.com.cn>
2018-05-29 16:26:45 +0800

    spring整合shiro

---
+++ dao/src/main/java/com/general/ww.java
@@ -0,0 +1,19 @@
+package com.general;
+
+/************************************************************************************
+ * Copyright (c) 2017 © Bestpay Co., Ltd.  All Rights Reserved.
+ * This software is published under the terms of the Bestpay.
+ * Software License version 1.0, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.
+ * <p>
+ * File name:      
+ * Create on:      2018/5/29
+ * Author :        官红诚
+ * <p>
+ * ChangeList
+ * -----------------------------------------------------------------------------
+ * Date                Editor        ChangeReasons
+ * 2018/5/29            官红诚         Create
+ ************************************************************************************/
+public class ww {
+}
